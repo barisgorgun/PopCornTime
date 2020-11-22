37a02bc2bf4db977bd2539ea3a5b287004c4a5bf
macbook <barisgorgunn@gmail.com>
2020-11-22 12:11:30 +0300

    Initial Commit

---
+++ PopCornTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PopCornTime
+//
+//  Created by MacBook on 22.11.2020.
+//  Copyright © 2020 Baris Gorgun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
